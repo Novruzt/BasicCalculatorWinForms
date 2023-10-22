489d52f9e50ec5d58ec29b991404df1a7df0459d
Novruz <novruztarverdiyev1@gmail.com>
2023-10-22 19:27:59 +0400

    Keyboard for dot is added. Code lines decreased.

--- BasicCalculator/Main.cs
+++ BasicCalculator/Main.cs
@@ -17,21 +17,26 @@ namespace BasicCalculator
     {
         private double Num;
         private double Result = 0;
-        private bool Status;
+        private bool IsSecond = false;
         private string Last;
         private bool FirstEqu = true;
         private bool IsFirst = true;
 
+
         public Main()
         {
+
             InitializeComponent();
 
-
             this.KeyPreview = true;
+
             this.KeyPress += new KeyPressEventHandler(BtnNumber_KeyPress);
             this.KeyPress += new KeyPressEventHandler(ClearPress_Key);
             this.KeyPress += new KeyPressEventHandler(RemoveKey_Press);
             this.KeyPress += new KeyPressEventHandler(ResultKey_Press);
+            this.KeyPress += new KeyPressEventHandler(DotKey_Press);
+
+
 
         }
 
@@ -45,26 +50,29 @@ namespace BasicCalculator
 
             else
             {
-                if (Status)
+                if (IsSecond)
                 {
                     LabelShow.Text = btn.Text;
-                    Status = false;
+                    IsSecond = false;
                 }
 
                 else
                     LabelShow.Text += btn.Text;
             }
+
+            Num = Convert.ToDouble(LabelShow.Text);
         }
 
         private void BtnOperation_Click(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
-            Status = true;
+            IsSecond = true;
 
             if (!IsFirst)
             {
-                BtnResult_Click(sender, e);
                 Last = btn.Text;
+                BtnResult_Click(sender, e);
+
             }
 
             else
@@ -87,6 +95,7 @@ namespace BasicCalculator
                         {
                             Result -= Convert.ToDouble(LabelShow.Text);
                             LabelShow.Text = Result.ToString();
+
                         }
 
                         Last = "-";
@@ -143,8 +152,11 @@ namespace BasicCalculator
             {
                 LabelShow.Text = "0";
                 Result = 0;
-                Status = false;
+                IsSecond = false;
                 FirstEqu = false;
+                Num = 0;
+                Last = "";
+                IsFirst = true;
             }
         }
 
@@ -155,13 +167,12 @@ namespace BasicCalculator
 
             if (LabelShow.Text.Length == 0)
                 LabelShow.Text = "0";
-
         }
 
         private void BtnDot_Click(object sender, EventArgs e)
         {
-            if (!LabelShow.Text.Contains("."))
-                LabelShow.Text += ".";
+            if (!LabelShow.Text.Contains(","))
+                LabelShow.Text += ",";
         }
 
         private void BtnResult_Click(object sender, EventArgs e)
@@ -214,30 +225,11 @@ namespace BasicCalculator
 
             LabelShow.Text = Result.ToString();
 
-        }
-
-        private void BtnNumber_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            if (Char.IsDigit(e.KeyChar))
-                if (LabelShow.Text == "0")
-                    LabelShow.Text = e.KeyChar.ToString();
-                else
-                {
-                    if (Status)
-                    {
-                        LabelShow.Text = e.KeyChar.ToString();
-                        Status = false;
-                    }
-                    else
-                        LabelShow.Text += e.KeyChar.ToString();
-                }
-
 
         }
 
-        private void BtnSignChanger_Click_Click(object sender, EventArgs e)
+        private void BtnSignChange_Click_Click(object sender, EventArgs e)
         {
-
             if (!LabelShow.Text.Contains("-"))
             {
                 if (LabelShow.Text == "0")
@@ -258,28 +250,40 @@ namespace BasicCalculator
 
         }
 
+        private void BtnNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsDigit(e.KeyChar))
+            {
+                if (LabelShow.Text == "0")
+                    LabelShow.Text = e.KeyChar.ToString();
+                else
+                {
+                    if (IsSecond)
+                    {
+                        LabelShow.Text = e.KeyChar.ToString();
+                        IsSecond = false;
+                    }
+                    else
+                        LabelShow.Text += e.KeyChar.ToString();
+                }
+
+                Num = Convert.ToDouble(LabelShow.Text);
+            }
+                
+
+          
+        }
+
         private void ClearPress_Key(object sender, KeyPressEventArgs e)
         {
             if (e.KeyChar == 'C' || e.KeyChar == 'c')
-                if (LabelShow.Text != "0")
-                {
-                    LabelShow.Text = "0";
-                    Result = 0;
-                    Status = false;
-                    FirstEqu = false;
-                }
+                BtnClear_Click(BtnClear, e);
         }
 
         private void RemoveKey_Press(object sender, KeyPressEventArgs e)
         {
             if (e.KeyChar == (char)Keys.Back)
-            {
-                if (LabelShow.Text != "0" && LabelShow.Text.Length != 0)
-                    LabelShow.Text = LabelShow.Text.Remove(LabelShow.Text.Length - 1);
-
-                if (LabelShow.Text.Length == 0)
-                    LabelShow.Text = "0";
-            }
+                BtnRemove_Click(BtnRemove, e);
         }
 
         private void OperationKey_Down(object sender, KeyEventArgs e)
@@ -290,35 +294,43 @@ namespace BasicCalculator
                 Last = "+";
                 BtnOperation_Click(BtnPlus, e);
             }
-                
 
             if (e.KeyCode == Keys.Subtract || e.KeyCode == Keys.OemMinus)
             {
                 Last = "-";
                 BtnOperation_Click(BtnMinus, e);
             }
-                
+
 
             if (e.Shift && e.KeyCode == Keys.D8)
             {
                 Last = "X";
                 BtnOperation_Click(BtnMultiply, e);
             }
-               
+
 
             if (e.KeyCode == Keys.Oem2)
             {
                 Last = "/";
                 BtnOperation_Click(BtnDivide, e);
             }
-               
 
+            if (e.KeyCode == Keys.Enter)
+            {
+                BtnResult_Click(BtnResult, e);
+            }
         }
 
         private void ResultKey_Press(object sender, KeyPressEventArgs e)
         {
-            if(e.KeyChar=='=')
+            if (e.KeyChar == '=' || e.KeyChar == (char)Keys.Enter)
                 BtnResult_Click(BtnResult, e);
+        }
+
+        private void DotKey_Press(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '.' || e.KeyChar == ',')
+                BtnDot_Click(BtnDot, e);
         }
     }
 }
